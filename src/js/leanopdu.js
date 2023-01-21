3e4664154be70784a3c1e49e98268436928d2d4a
Komeil Majidi <k.majidi@esmcell.com>
2023-01-22 03:07:52 +0330

    int to hex

--- src/js/leanopdu.js
+++ src/js/leanopdu.js
@@ -29,34 +29,76 @@ function intToBin(x, size) {
 	return bin;
 }
 
-function HexToNum(numberS) 
-{
-	var tens = MakeNum(numberS.substring(0,1));
-	
+function HexToNum(numberS) {
+	var tens = MakeNum(numberS.substring(0, 1));
+
 	var ones = 0;
-	if(numberS.length > 1)
-		ones=MakeNum(numberS.substring(1,2));
-	if(ones == 'X') 
-	{
+	if (numberS.length > 1)
+		ones = MakeNum(numberS.substring(1, 2));
+	if (ones == 'X') {
 		return "00";
 	}
-	return  (tens * 16) + (ones * 1);
+	return (tens * 16) + (ones * 1);
 }
 
-function MakeNum(str) 
-{
-	if((str >= '0') && (str <= '10')) //check
+function MakeNum(str) {
+	if ((str >= '0') && (str <= '10')) //check
 		return str;
-	switch(str.toUpperCase()) 
-	{
-		case "A": return 10;
-		case "B": return 11;
-		case "C": return 12;
-		case "D": return 13;
-		case "E": return 14;
-		case "F": return 15;
+	switch (str.toUpperCase()) {
+		case "A":
+			return 10;
+		case "B":
+			return 11;
+		case "C":
+			return 12;
+		case "D":
+			return 13;
+		case "E":
+			return 14;
+		case "F":
+			return 15;
 		default:
-		return 16;
-   	}
+			return 16;
+	}
 	return 16;
+}
+
+function intToHex(i) 
+{
+	var sHex = "0123456789ABCDEF";
+	h = "";
+	i = parseInt(i);
+	for (j = 0; j <= 3; j++) {
+		h += sHex.charAt((i >> (j * 8 + 4)) & 0x0F) +
+			sHex.charAt((i >> (j * 8)) & 0x0F);
+	}
+	return h.substring(0, 2);
+}
+
+function ToHex(i) {
+	var sHex = "0123456789ABCDEF";
+	var Out = "";
+	Out = sHex.charAt(i & 0xf);
+	i >>= 4;
+	Out = sHex.charAt(i & 0xf) + Out;
+	return Out;
+}
+
+function getSevenBit(character) 
+{
+	for (var i = 0; i < sevenbitdefault.length; i++) {
+		if (sevenbitdefault[i] == character) {
+			return i;
+		}
+	}
+	alert("No 7 bit char for " + character);
+	return 0;
+}
+
+function getEightBit(character) {
+	return character;
+}
+
+function get16Bit(character) {
+	return character;
 }
