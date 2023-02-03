ed6dbc047e628c513098d0af21712a196d67d4fe
Komeil Majidi <k.majidi@esmcell.com>
2023-02-03 21:52:23 +0330

    encode/decode test front

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+function hiddenbox () {
+for ( var i=0;i<=1;i++ ) {
+document.querySelectorAll("section")[i].style.display="none"
+}		
+}
+
+function displaybox () {
+for ( var i=0;i<=1;i++ ) {
+document.querySelectorAll("section")[i].style.display="flex"
+}		
+}
