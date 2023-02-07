a69ea8ce4641409a88c6c937a2a048c7a062de3a
Komeil Majidi <k.majidi@esmcell.com>
2023-02-07 16:52:23 +0330

    pdu decoder UI Function

--- src/index.js
+++ src/index.js
@@ -10,3 +10,31 @@ for ( var i=0;i<=1;i++ ) {
 document.querySelectorAll("section")[i].style.display="flex"
 }		
 }
+
+
+function decodepduUI () {
+	
+	var decodetextarea = document.getElementById("decodetextarea").value;
+	var result = pduDecoder(decodetextarea);
+	Swal.fire({
+  icon: 'success',
+  title: 'Decoded PDU',
+  html:`<table>
+        <tbody>
+            <tr>
+                <td>`+result[0]+`</td>
+            </tr>
+			<tr>
+                <td>`+result[1]+`</td>
+            </tr>
+			<tr>
+                <td>`+result[2]+`</td>
+            </tr>
+			 <tr>
+                <td>`+result[3]+`</td>
+            </tr>
+</tbody>
+</table>`
+})
+	
+}
